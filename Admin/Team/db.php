2410f9712d26f53381205f0134d5c01a4142f7e7
amrutha6677 <123219926+amrutha6677@users.noreply.github.com>
2023-11-05 21:35:20 -0500

    Create db.php

---
+++ Admin/Team/db.php
@@ -0,0 +1,17 @@
+<?php
+include 'db.php'; 
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Create Team Member</title>
+</head>
+<body>
+    <h1>Create Team Member</h1>
+
+    <form method="post" action="edit.php">
+        <button type="submit" name="create">Create Team Member</button>
+    </form>
+</body>
+</html>
